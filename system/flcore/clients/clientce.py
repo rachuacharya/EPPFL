8d2e6fa0894fdc873cf2a49edf5c9b89fbf07aad
RachuAcharya <073bex429.rachu@pcampus.edu.np>
2024-05-23 15:56:39 -0400

    Add comments to Client Training

--- system/flcore/clients/clientce.py
+++ system/flcore/clients/clientce.py
@@ -22,6 +22,9 @@ class clientCE(Client):
         self.mu = 0.001
 
     def train(self):
+        """
+        This is where the main Client Model Training happens
+        """
         trainloader = self.load_train_data()
 
         start_time = time.time()
@@ -38,30 +41,46 @@ class clientCE(Client):
             self.compressed_model.unpack(copy.deepcopy(self.model), self.device))
 
         # self.model.to(self.device)
+
+        # Set Model to Training Mode
         self.model.train()
 
+        #  Create a deep copy of the model to use as a fixed reference for regularization during training.
         fixed_model = copy.deepcopy(self.model)
 
         max_local_steps = self.local_steps
 
         for step in range(max_local_steps):
             for i, (x, y) in enumerate(trainloader):
+                # Move data to specified Device
                 if type(x) == type([]):
                     x[0] = x[0].to(self.device)
                 else:
                     x = x.to(self.device)
                 y = y.to(self.device)
+
+                # Zeros the gradients of the optimizer.
                 self.optimizer.zero_grad()
+
+                # Pass the input x through the model to get the output.
                 output = self.model(x)
+
+                # Compute the cross-entropy loss 
                 ce_loss = self.loss(output, y)
 
+                # Compute the regularization loss
                 reg_loss = 0
                 fixed_params = {n: p for n, p in fixed_model.named_parameters()}
                 for n, p in self.model.named_parameters():
                     reg_loss += ((p - fixed_params[n].detach()) ** 2).sum()
 
+                # Combine the cross-entropy loss and regularization loss into a total loss.
                 loss = self.alpha * ce_loss + 0.5 * self.mu * reg_loss
+
+                # Backpropagate the loss and updates the model parameters using the optimizer.
                 loss.backward()
+
+                # Update the model parameters using the computed gradients.
                 self.optimizer.step()
 
         self.compressed_model = Packages()
