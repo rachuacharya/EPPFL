9138207ffdc28c976fbfef3558163fa869f6158c
RachuAcharya <073bex429.rachu@pcampus.edu.np>
2024-09-22 22:46:37 -0400

    validation path in strip_enc

--- system/main.py
+++ system/main.py
@@ -47,14 +47,8 @@ def run(args):
                 args.model = ResNet(BasicBlock, [2,2,2,2], num_classes = 10).to(args.device)
         elif model_str == "resnet":
             # Resnet for Tranfer Learning
-<<<<<<< Updated upstream
-            args.model = torchvision.models.resnet18(pretrained=False, num_classes=args.num_classes).to(args.device)
-
-=======
-            print("Using Resnet Model...")
             # args.model = torchvision.models.resnet18(pretrained=False, num_classes=args.num_classes).to(args.device)
             args.model = Resnet9(3, 10).to(args.device)
->>>>>>> Stashed changes
         else:
             raise NotImplementedError
 
